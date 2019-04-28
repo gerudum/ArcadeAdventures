477e08e7e971fc5d8d5e541d17ae6437d765f59c
gerudum <49799704+gerudum@users.noreply.github.com>
2019-04-28 15:26:30 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-require('http').createServer().listen(3000)
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
 
 
 var
